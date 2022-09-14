0fdff209f63ce199ef6c92279669ab62f09c83a2
HarryTHT <hxanders101@gmail.com>
2022-09-14 10:27:43 +0100

    Create Task3.py

---
+++ Task3.py
@@ -0,0 +1,17 @@
+
+# getting number
+number = int(input("Number between 1 and 3"))
+
+# is it within the range?
+for a in range(10000000): #how may times it would repeat,
+    if number >= 1 and number <= 3:
+        continue
+    else:
+        number = 0
+        number = int(input("option not didn't work, choose a new number"))
+        
+#out of the for loop
+print("you chose option ", number)
+
+
+
