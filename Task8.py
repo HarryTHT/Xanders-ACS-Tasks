5af29db9d1401baab94bdbb59a60eab8fe566595
HarryTHT <hxanders101@gmail.com>
2022-09-16 11:58:56 +0100

    Create Task8.py

---
+++ Task8.py
@@ -0,0 +1,11 @@
+#getting values
+value1 = int(input())
+value2 = int(input())
+
+#evaluating which one is larger
+if value1 > value2:
+  print(value2,value1)
+elif value1 < value2:
+  print(value1,value2)
+elif value1 == value2:
+  print("they are equal")
