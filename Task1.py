b9b345bb9d16dad798bca4752173c14593289286
HarryTHT <hxanders101@gmail.com>
2022-09-16 11:39:52 +0100

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,6 +1,6 @@
 #Getting the user input
-first_name = input(print("What is your first name?"))
-second_name = input(print("What is your second name?"))
+first_name = input()
+second_name = input()
 
 #Printing the full name
 print("your whole name is: "+first_name+" "+second_name)
