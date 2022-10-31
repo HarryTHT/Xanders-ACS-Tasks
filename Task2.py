878fb56613a296b2cb48a90b348010d9fe11694b
HarryTHT <hxanders101@gmail.com>
2022-10-31 10:42:34 +0000

    Delete Task2.py

--- Task2.py
+++
@@ -1,7 +0,0 @@
-#getting user input
-password = str(input(print("Enter a Password")))
-
-if len(password)>6: #validating wether it is too long or short
-    print("Password is valid")
-else:
-    print("password is too short")
